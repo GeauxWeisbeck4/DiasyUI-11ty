3008e0a373ffc0388f8b07bb64731ca4887cea59
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2023-04-22 15:48:13 -0400

    2 files modified, 5 added, 2 deleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   content: ['./src/**/*.{njk,md}'],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#69c120",                 
+          "secondary": "#c141fc",                 
+          "accent": "#5deaab",                 
+          "neutral": "#271A2E",                  
+          "base-100": "#2B3A5F",                
+          "info": "#5D85E9",                
+          "success": "#64E8BE",                
+          "warning": "#936110",                
+          "error": "#F86863",
+        },
+      },
+    ],
+  },
   plugins: [require('daisyui'), require('@tailwindcss/typography')],
 };
